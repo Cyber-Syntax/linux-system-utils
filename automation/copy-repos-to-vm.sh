bf0837a8130b0c2a47f96ddc0bae682d9dd56676
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-12-23 14:06:53 +0300

    feat: add linux-system-utils to copy script

--- automation/copy-repos-to-vm.sh
+++ automation/copy-repos-to-vm.sh
@@ -3,6 +3,7 @@
 # Script to copy source code to a virtual machine
 source_dir=~/Documents/my-repos/my-unicorn
 source_dir2=~/Documents/my-repos/auto-penguin-setup
+source_dir4=~/Documents/my-repos/linux-system-utils
 shared_folder_dir=/mnt/sda2/virt-manager-share/shared-folder
 
 # Copy the source code to the shared folder with rsync
@@ -10,3 +11,4 @@ shared_folder_dir=/mnt/sda2/virt-manager-share/shared-folder
 
 rsync -avz $source_dir/ $shared_folder_dir/my-unicorn/
 rsync -avz $source_dir2/ $shared_folder_dir/auto-penguin-setup/
+rsync -avz $source_dir4/ $shared_folder_dir/linux-system-utils/

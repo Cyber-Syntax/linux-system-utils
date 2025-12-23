e9810d0b8ee8a1e6edf570aab3b68de7d18e690c
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-12-23 14:09:05 +0300

    fix: correct shebang and add copyright

--- package-management/arch-app-install.sh
+++ package-management/arch-app-install.sh
@@ -1,6 +1,35 @@
-#!/bin/usr/env bash
+#!/usr/bin/env bash
+# Copyright (c) 2025, Cyber-Syntax Serif
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 #ARCH LINUX
-# sh script to install my apps
+#script to install my apps
 
 packages=(
   # Essentials
@@ -118,10 +147,10 @@ yay_packages=(
 )
 
 # pacman install packages
-for package in ${packages[@]}; do
+for package in "${packages[@]}"; do
   echo "Installing $package"
   # error handling
-  if ! sudo pacman -S --noconfirm --needed $package; then
+  if ! sudo pacman -S --noconfirm --needed "$package"; then
     echo "Error installing $package"
   else
     echo "$package installed"
@@ -129,10 +158,10 @@ for package in ${packages[@]}; do
 done
 
 # yay install packages
-for yay_package in ${yay_packages[@]}; do
+for yay_package in "${yay_packages[@]}"; do
   echo "Installing $yay_package"
   # error handling
-  if ! yay -S --noconfirm --needed $yay_package; then
+  if ! yay -S --noconfirm --needed "$yay_package"; then
     echo "Error installing $yay_package"
   else
     echo "$yay_package installed"
